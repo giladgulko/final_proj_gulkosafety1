646837682dc9978645260e71262cf93b5fba888d
giladgulko <giladgulko@gmail.com>
2021-03-14 11:13:55 +0200

    new proj
    new proj

---
+++ final_proj_gulkosafety/final_proj_gulkosafety/Models/test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace final_proj_gulkosafety.Models
+{
+    public class test
+    {
+        int id;
+        string name;
+
+        public test(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
